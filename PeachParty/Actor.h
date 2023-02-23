0d6209fe0acf56cac3564d2826aac15dcdd18e73
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-02-23 12:28:33 -0800

    Added classes to Actor w/ some private/public members that have yet to be implemented.

--- PeachParty/Actor.h
+++ PeachParty/Actor.h
@@ -3,6 +3,31 @@
 
 #include "GraphObject.h"
 
-// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class Actor: public GraphObject{
+  public:
+    Actor();
+    void virtual doSomething() = 0;
+  private:
+    int spriteDirection; //Sprite Direction
+};
+
+class Peach: public Actor{
+  public:
+    Peach();
+  private:
+    int walkDirection; //Walk Direction
+    int pNum; //Player Number
+    int state; //Walking or Waiting State
+    int tTMove; //Ticks to Move
+};
+
+class Coin: public Actor{
+  public:
+    Coin();
+    bool isActive(); //Checks if coin square is alive or dead
+  private:
+    bool aliveStatus;
+    int numCoins; //Number of coins to give/take
+};
 
 #endif // ACTOR_H_
